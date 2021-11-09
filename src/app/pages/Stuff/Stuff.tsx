373cbee773f6ad8b3ba72beb6620d5de24d55b03
Samet Uslu <samet_u@hotmail.de>
2021-11-09 15:25:06 +0100

    npm install style components

--- src/app/pages/Stuff/Stuff.tsx
+++ src/app/pages/Stuff/Stuff.tsx
@@ -3,5 +3,5 @@ import { useParams } from 'react-router';
 
 export default function Stuff(): JSX.Element {
   const params = useParams();
-  return <h2>{params.stuff}</h2>;
+  return <h2> Stuff: {params.stuff}</h2>;
 }

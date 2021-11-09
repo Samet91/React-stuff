ad4bdb93a184b3b64c95f8d7e8887930655479a6
Samet Uslu <samet_u@hotmail.de>
2021-11-09 14:25:19 +0100

    Add useParams

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Stuff from './pages/Stuff/Stuff';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
+      Header
+      <Link to="/about">--About link--</Link>
       <Routes>
         <Route path="/" element={<main>Home</main>} />
 
         <Route path="/about" element={<main>About</main>} />
 
-        <Route path="/me" element={<main>About me</main>} />
+        <Route path="/me" element={<Stuff />}>
+          <Route path=":stuff" element={<Stuff />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/app/pages/Stuff/Stuff.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useParams } from 'react-router'; 
+
+export default function Stuff(): JSX.Element {
+ const params = useParams() ;
+ return <h2>{params.stuff}</h2>
+}
